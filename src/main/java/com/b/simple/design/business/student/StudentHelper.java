5bef134c3c0992e0dfb3be7faa47f0df4a11c911
rmccoy4145 <ryan.mccoy85@gmail.com>
2021-05-07 18:22:56 -0400

    refractor of getGrade & isGradeB

--- src/main/java/com/b/simple/design/business/student/StudentHelper.java
+++ src/main/java/com/b/simple/design/business/student/StudentHelper.java
@@ -1,13 +1,30 @@
 package com.b.simple.design.business.student;
 public class StudentHelper {
-
+    private static final int UPPER_LIMIT_GRADEB = 80;
+    private static final int MATHS_UPPER_INCREASE = 10;   
+    private static final int LOWER_LIMIT_GRADE_B = 51; 
+    
+    
+    private static final String GRADE_B = "B";
+    private static final String GRADE_A = "A";
+    private static final String GRADE_C = "C";
+    private static final int LOWER_LIMIT_GRADE_A = 91; 
+    private static final int MATHS_LOWER_INSCREASE = 5; 
 	/* PROBLEM 1 */	
 	/*
 	* You get a grade B if marks are between 51 and 80 (both inclusive). Except for Maths where the upper limit is increased by 10.
 	*/
 	public boolean isGradeB(int marks, boolean isMaths) {
-		return isMaths ? marks>=51 && marks<=90 : marks>=51 && marks<=80; 
+            int increase = isMaths ? MATHS_UPPER_INCREASE : 0;
+                    
+            int upperLimter = UPPER_LIMIT_GRADEB + increase;
+            
+            return marks>=LOWER_LIMIT_GRADE_B && marks<=upperLimter;
+
 	}
+ 
+
+
 
 	/* PROBLEM 2 */
 	/*
@@ -16,27 +33,19 @@ public class StudentHelper {
 	Except for Maths where marks to get a Grade are 5 higher than required for other subjects.
 	*/
 
+ 
+   
 	public String getGrade(int mark, boolean isMaths) {
-		String grade = "C";
 		
-		if (isGradeA(mark, isMaths))
-			grade = "A";
-		else if (isBGrade(mark, isMaths)) {
-			grade = "B";
-		}
-		return grade;
-	}
-
-	private boolean isGradeA(int mark, boolean isMaths) {
-		int lowerLimitForAGrade = isMaths ? 95
-				: 90;
-		return mark > lowerLimitForAGrade;
-	}
-
-	private boolean isBGrade(int mark, boolean isMaths) {
-		int lowerLimitGradeB = isMaths ? 55
-				: 50;
-		return mark > lowerLimitGradeB && mark < 90;
+                int mathsLowerIncrease = isMaths ? MATHS_LOWER_INSCREASE : 0;
+                
+		if (mark >= LOWER_LIMIT_GRADE_A + mathsLowerIncrease)
+			return GRADE_A;
+                
+		if (mark >= LOWER_LIMIT_GRADE_B + mathsLowerIncrease)
+			return GRADE_B;
+		
+		return GRADE_C;
 	}
 
     /*  PROBLEM 3
@@ -58,8 +67,8 @@ public class StudentHelper {
     public String willQualifyForQuiz(int marks1, int marks2, boolean isMaths) {
         if ((isMaths ? marks1 <= 25 : marks1 <= 20)
                 || (isMaths ? marks2 <= 25 : marks2 <= 20)) return "NO";
-        if ((isMaths ? marks1 >= 85 : marks1 >= 80)
-                || (isMaths ? marks2 >= 85 : marks2 >= 80)) return "YES";
+        if ((isMaths ? marks1 >= 85 : marks1 >= UPPER_LIMIT_GRADEB)
+                || (isMaths ? marks2 >= 85 : marks2 >= UPPER_LIMIT_GRADEB)) return "YES";
         return "MAYBE";
     }	
 
