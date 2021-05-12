f530934f54e4d6963ec75ead735d7eefa014660b
rmccoy4145 <ryan.mccoy85@gmail.com>
2021-05-12 17:25:43 -0400

    refractored movie

--- src/main/java/com/c/refactoring/movie/Movie.java
+++ src/main/java/com/c/refactoring/movie/Movie.java
@@ -1,6 +1,9 @@
 package com.c.refactoring.movie;
 
 import com.c.refactoring.StringUtils;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Movie {
 
@@ -19,19 +22,32 @@ public class Movie {
     Where x represents any digit between 0 and 9, and y represents 
     any digit between 1 and 4*/
     public boolean isValidRating() {
-        if (this.getRating() != null) {
-            if (this.getRating().substring(0, 1).equalsIgnoreCase("B")
-                    && this.getRating().length() == 2) {
-                if (StringUtils.isNumeric(this.getRating().substring(1, 2))
-                        && Integer.parseInt(this.getRating().substring(1, 2)) > 0
-                        && Integer.parseInt(this.getRating().substring(1, 2)) < 5)
-                    return true;
+        final String currentRating = this.getRating();
+        if(currentRating == null) return false;
 
-            } else if (this.getRating().substring(0, 1).equalsIgnoreCase("A")
-                    && this.getRating().length() == 3
-                    && StringUtils.isNumeric(this.getRating().substring(1, 3)))
-                return true;
+        if (validBRating(currentRating)) return true;
+        if (validARating(currentRating)) return true;
+        
+        return false;
+    }
 
+    private boolean validBRating(final String currentRating) throws NumberFormatException
+    {
+        List<String> validRatings = Arrays.asList("B1", "B2", "B3", "B4");
+        
+        if (validRatings.contains(currentRating.toUpperCase())) return true;
+
+        return false;
+    }
+
+    private boolean validARating(final String currentRating)
+    {
+        String firstCharacter = currentRating.substring(0, 1);
+        if (firstCharacter.equalsIgnoreCase("A")
+                && currentRating.length() == 3
+                && StringUtils.isNumeric(currentRating.substring(1, 3)))
+        {
+            return true;
         }
         return false;
     }
