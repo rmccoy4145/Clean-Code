b04de4be23f43d59e48a4ade8e0700513e3e72d3
rmccoy4145 <ryan.mccoy85@gmail.com>
2021-05-12 18:59:32 -0400

    refractored menuItem

--- src/main/java/com/c/refactoring/menuexamples/MenuAccess.java
+++ src/main/java/com/c/refactoring/menuexamples/MenuAccess.java
@@ -1,34 +1,47 @@
 package com.c.refactoring.menuexamples;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MenuAccess {
 
     public void setAuthorizationsInEachMenus(
-            List<MenuItem> menuItemsList, Role[] roles) {
-        for (int i = 0; i < menuItemsList.size(); i++) {
-            MenuItem menuItem = menuItemsList.get(i);
-            if (roles != null) {
-                for (int j = 0; j < roles.length; j++) {
-                    if (roles[j].getName().equals(menuItem
-                            .getReadAccessRole())
-                            && !Constants.WRITE.equals(
-                                    menuItem
-                                            .getAccess())) {
-                        menuItem.setAccess(Constants.READ);
-                        menuItem.setVisible(true);
-                    } else if (roles[j].getName().equals(
-                            menuItem
-                                    .getWriteAccessRole())) {
-                        menuItem.setAccess(Constants.WRITE);
-                        menuItem.setVisible(true);
-                    }
-                }
+            List<MenuItem> menuItemsList, Role[] roles)
+    {
+        if (roles == null) return;
+        
+        
+        menuItemsList.forEach((menuItem) ->
+        {
+               authorizeMenuItem(roles, menuItem);
+        });
 
+    }
+
+    private void authorizeMenuItem(Role[] roles, MenuItem menuItem)
+    {
+        final String readAccessRole = menuItem.getReadAccessRole();
+        final String writeAccessRole = menuItem.getWriteAccessRole();
+        Arrays.asList(roles).stream().forEach((role) ->
+        {
+            final String roleName = role.getName();
+            if (roleName.equals(readAccessRole))
+            {
+                authorizeMenuItemAccess(menuItem, Constants.READ);
+  
             }
+            
+            if (roleName.equals(writeAccessRole))
+            {
+                authorizeMenuItemAccess(menuItem, Constants.WRITE);
+            }
+        });
+    }
 
-        }
-
+    private void authorizeMenuItemAccess(MenuItem menuItem, String accessType)
+    {
+                        menuItem.setAccess(accessType);
+                        menuItem.setVisible(true);
     }
 
 }
